15f8061237b990a493e71e65ed3b18a671dbc041
Maranaho N'Guessan <maranahonguessan@Maranahos-MacBook-Pro.local>
2024-10-23 15:02:20 +0100

    fix(web-1025: commenting out useAddUser hook)

--- src/components/AddUserForm/AddUserForm.tsx
+++ src/components/AddUserForm/AddUserForm.tsx
@@ -1,26 +1,26 @@
 import { FC } from 'react'
-import { Gender } from "../../types/userTypes"
-import { useAddUser } from "../../hooks"
+// import { Gender } from "../../types/userTypes"
+// import { useAddUser } from "../../hooks"
 
 
 const AddUserForm: FC = () => {
 
     // Use the custom hook for adding users
-    const { mutate: addUser } = useAddUser()
+    // const { mutate: addUser } = useAddUser()
 
     //Just testing CRUDS better UX later
     // Temporary function to prompt user input for a new user
-    const handleAddUser = () => {
-        const firstName = prompt("Enter first name:")
-        const lastName = prompt("Enter last name:")
-        const ageString = prompt("Enter age:")
+    // const handleAddUser = () => {
+    //     const firstName = prompt("Enter first name:")
+    //     const lastName = prompt("Enter last name:")
+    //     const ageString = prompt("Enter age:")
 
-        if (firstName && lastName && ageString) {
-            const age = parseInt(ageString, 10)
-            if (!isNaN(age)) addUser({ firstName, lastName, age, gender: Gender.Male })
-            else  alert("Please enter a valid age.")
-        }
-    }
+    //     if (firstName && lastName && ageString) {
+    //         const age = parseInt(ageString, 10)
+    //         if (!isNaN(age)) addUser({ firstName, lastName, age, gender: Gender.Male })
+    //         else  alert("Please enter a valid age.")
+    //     }
+    // }
 
     return (
         <div>
