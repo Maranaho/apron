d1d6812584528036b4ed9eae89383f13a1125d7e
Maranaho N'Guessan <maranahonguessan@Maranahos-MacBook-Pro.local>
2024-10-24 01:00:09 +0100

    feat(web-1039: Chat done)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useLocation, useParams } from "react-router-dom"
 import Users from './components/Users/Users'
 import Header from './components/Header/Header'
 import AddUserForm from './components/AddUserForm/AddUserForm'
+import Chat from './components/Chat/Chat'
 import DeleteUserModal from './components/DeleteUserModal/DeleteUserModal'
 import Modal from './components/Modal/Modal'
 import styles from "./styles/App.module.scss"
@@ -22,6 +23,7 @@ const App:FC = ()=> {
         <Users />
         {(openAddNewUser || openEditUser) && <Modal><AddUserForm/></Modal>}
         {openDeleteUser && <Modal><DeleteUserModal/></Modal>}
+        <Chat/>
     </main>
   )
 }
